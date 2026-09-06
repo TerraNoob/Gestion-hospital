e2d9a77f68fcc4d5960fabfe730d43285ff9328a
TerraNoob <cripencio.polvorencio@gmail.com>
2026-09-06 17:24:15 -0300

    Clase Paciente, falta metodo obtenerGravedad

--- src/modelo/paciente.java
+++ src/modelo/paciente.java
@@ -1,5 +1,56 @@
 package modelo;
 
 public class paciente {
-
+	private String nombre;
+	private String rut;
+	private int edad;
+	private float gravedad;
+	private SignosVitales signos;
+	private Cama camaActual;
+	
+	public Paciente(String nombre, String rut, int edad, SignosVitales signos, Cama camaActual) {
+		this.nombre = nombre;
+		this.rut = rut;
+		this.edad = edad;
+	this.gravedad = obtenerGravedad(signos);
+	this.signos = signos;
+	this.camaActual = camaActual;
+	}
+	
+	public String getNombre(){
+		return this.nombre;
+	}
+	public String getRut() {
+		return this.rut;
+	}
+	public int getEdad() {
+		return this.edad;
+	}
+	public float getGravedad() {
+		return this.gravedad;
+	}
+	public SignosVitales getSignos() {
+		return signos;
+	}
+	public Cama getCama() {
+		return this.camaActual;
+	}
+	
+	public void setNombre(String n) {
+		this.nombre = n;
+	}
+	public void setRut(String r) {
+		this.rut = r;
+	}
+	public void setEdad(int e) {
+		this.edad = e;
+	}
+	public void setSignosVitales(SignosVitales signos) {
+		this.signos = signos;
+		this.gravedad = obtenerGravedad(signos);
+	}
+	public void setCama(Cama camaActual) {
+		this.camaActual = camaActual;
+	}
+	
 }
