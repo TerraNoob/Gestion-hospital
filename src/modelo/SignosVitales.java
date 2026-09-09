d082800e2cea5ea5991ef7c55f4a5b52cf1f39a4
Martín <martin.lira.rodriguez@gmail.com>
2026-09-08 21:04:53 -0300

    implementado el método calcularGravedad
    Added a method to calculate the severity based on vital signs.

--- src/modelo/SignosVitales.java
+++ src/modelo/SignosVitales.java
@@ -1,6 +1,4 @@
-package modelo;
-
-public class SignosVitales {
+public class SignosVitales{
     private int frecuenciaCardiaca;
     private double presionArterial;
     private int saturacion;
@@ -13,6 +11,26 @@ public class SignosVitales {
         this.temperatura = temperatura;
     }
 
+    public float calcularGravedad(){
+        float gravedad = 0.0f;
+
+        // Considerado saturación bajo lo normal
+        if (this.saturacion < 95){
+            gravedad += 2.0f;
+        }
+
+        // Considerado frecuencia cardíaca sobre y bajo lo normal
+        if (this.frecuenciaCardiaca >= 100 || this.frecuenciaCardiaca < 50){
+            gravedad += 2.0f;
+        }
+
+        // Considerado temperatura bajo y sobre lo normal
+        if (this.temperatura >= 37.5 || this.temperatura < 35.5){
+            gravedad += 2.0f;
+        }
+        return gravedad;
+    }
+
     public int getFrecuenciaCardiaca(){ return frecuenciaCardiaca; }
 
     public double getPresionArterial(){ return presionArterial; }
@@ -21,15 +39,19 @@ public class SignosVitales {
 
     public double getTemperatura(){ return temperatura; }
 
-    public void setFrecuenciaCardiaca(int frecuenciaCardiaca){ 
-        this.frecuenciaCardiaca = frecuenciaCardiaca; }
+    public void setFrecuenciaCardiaca(int frecuenciaCardiaca){
+        this.frecuenciaCardiaca = frecuenciaCardiaca;
+    }
 
-    public void setPresionArterial(double presionArterial){ 
-        this.presionArterial = presionArterial; }
+    public void setPresionArterial(double presionArterial){
+        this.presionArterial = presionArterial;
+    }
 
-    public void setSaturacion(int saturacion){ 
-        this.saturacion = saturacion; }
+    public void setSaturacion(int saturacion){
+        this.saturacion = saturacion;
+    }
 
-    public void setTemperatura(double temperatura){ 
-        this.temperatura = temperatura; }
+    public void setTemperatura(double temperatura){
+        this.temperatura = temperatura;
+    }
 }
